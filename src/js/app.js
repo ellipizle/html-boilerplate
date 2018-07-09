4935cf1d7be62a3d82b81bfe250b567664841fb8
Olivier Bossel <olivier.bossel@gmail.com>
2018-07-09 13:05:23 +0200

    add standard js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 // import some dependencies like polyfills, etc...
-require("babel-polyfill");
-require('webcomponents.js/webcomponents-lite');
-require('cssuseragent');
-require('fastclick');
+require('babel-polyfill')
+require('webcomponents.js/webcomponents-lite')
+require('cssuseragent')
+require('fastclick')
 
 // main application entry point
-require('./webcomponent-import');
-require('./webcomponent-props');
-require('./main');
+require('./webcomponent-import')
+require('./webcomponent-props')
+require('./main')
